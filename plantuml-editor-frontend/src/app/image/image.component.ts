17cd2a3ed59dd487717337c5a6f294c38918a503
m_seic01 <m_seic01@uni-muenster.de>
2021-08-22 22:53:40 +0200

    #12

--- plantuml-editor-frontend/src/app/image/image.component.ts
+++ plantuml-editor-frontend/src/app/image/image.component.ts
@@ -1,13 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PlantumlHolder} from '../shared/plantuml-holder';
+import {FormControl} from '@angular/forms';
+import {Subscription} from 'rxjs';
+
+const ALLOW_OVERFLOW_LOCAL_STORAGE_KEY = 'hakenadu/plantuml-editor.allow-overflow';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
-export class ImageComponent {
+export class ImageComponent implements OnInit, OnDestroy {
+
+  allowOverflow = new FormControl(localStorage.getItem(ALLOW_OVERFLOW_LOCAL_STORAGE_KEY) === 'true');
+  private allowOverflowValueChangesSubscription?: Subscription;
 
   constructor(public plantumlHolder: PlantumlHolder) {
+
+  }
+
+  ngOnInit() {
+    this.allowOverflowValueChangesSubscription = this.allowOverflow.valueChanges
+      .subscribe(allowOverflow => localStorage.setItem(ALLOW_OVERFLOW_LOCAL_STORAGE_KEY, allowOverflow));
+  }
+
+  ngOnDestroy() {
+    this.allowOverflowValueChangesSubscription?.unsubscribe();
   }
 }
