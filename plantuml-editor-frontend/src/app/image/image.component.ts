81db7d5f70c5a0ae244600ecbfcf3fd84ec918cd
m_seic01 <m_seic01@uni-muenster.de>
2021-08-26 19:31:57 +0200

    #11 docker-compose webdav example

--- plantuml-editor-frontend/src/app/image/image.component.ts
+++ plantuml-editor-frontend/src/app/image/image.component.ts
@@ -1,10 +1,13 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PlantumlHolder} from '../shared/plantuml-holder';
 import {FormControl} from '@angular/forms';
-import {Subscription} from 'rxjs';
+import {Observable, of, Subscription} from 'rxjs';
 import {ConfigService} from '../shared/config.service';
 import {MatDialog} from '@angular/material/dialog';
 import {PermalinkComponent} from '../permalink/permalink.component';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {catchError, map} from 'rxjs/operators';
 
 const ALLOW_OVERFLOW_LOCAL_STORAGE_KEY = 'hakenadu/plantuml-editor.allow-overflow';
 
@@ -18,10 +21,17 @@ export class ImageComponent implements OnInit, OnDestroy {
   allowOverflow = new FormControl(localStorage.getItem(ALLOW_OVERFLOW_LOCAL_STORAGE_KEY) === 'true');
   private allowOverflowValueChangesSubscription?: Subscription;
 
+  permalinkSupported: Observable<boolean>;
+
   constructor(public plantumlHolder: PlantumlHolder,
               public configService: ConfigService,
-              private matDialog: MatDialog) {
-
+              private matDialog: MatDialog,
+              httpClient: HttpClient) {
+    this.permalinkSupported = httpClient.get(`${environment.backendUrl}/documents`)
+      .pipe(
+        map(() => true),
+        catchError(error => of(false))
+      );
   }
 
   ngOnInit() {
