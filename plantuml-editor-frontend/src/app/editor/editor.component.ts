1315731c6d9c4cb01d50bc53e3abf4b8dc8a8aa3
m_seic01 <m_seic01@uni-muenster.de>
2023-04-14 00:17:23 +0200

    new highlighting ruleset

--- plantuml-editor-frontend/src/app/editor/editor.component.ts
+++ plantuml-editor-frontend/src/app/editor/editor.component.ts
@@ -16,30 +16,54 @@ const CustomHighlightRules = function () {
    * NOTICE: The current rule set is a slightly adapted excerpt from https://github.com/sujoyu/plantuml-previewer
    */
   this.$rules = {
+    multilineNote: [
+      {
+        token: "keyword.control.source.wsd",
+        regex: /\bend note\b/,
+        next: "start"
+      },
+      {
+        token: "string.quoted.double.source.wsd",
+        regex: /.*$/
+      },
+      {
+        defaultToken: "string.quoted.double.source.wsd"
+      }
+    ],
     start: [
       {
+        token: "variable.source.wsd",
+        regex: /#[^\s]+/
+      },
+      {
+        token: "keyword.control.source.wsd",
+        regex: /(?:-|\.)+>|<\s*(?:-|\.)+|(?:-|\.)+(?:x|o|\||\[|]|>|\||\\)(?!\||>)|(?:-|\.)+\|>|(?:-|\.)+<\||<\|(?:-|\.)+|(?:-|\.)+(?!\||>)/,
+      },
+      {
+        token: "keyword.control.source.wsd",
+        regex: /{|}/,
+      },
+      {
+        token: "string.quoted.double.source.wsd",
+        regex: /(?:\:)(.*)$/
+      },
+      {
+        token: "keyword.control.source.wsd",
+        regex: /\bnote\b(?:(?!:[^:]*|as\s+\w).)*$/,
+        next: "multilineNote"
+      },
+      {
+        token: "keyword.control.source.wsd",
+        regex: /\bend note\b/,
+        next: "start"
+      },
+      {
         token: "comment.line.source.wsd",
-        regex: /^(?:#|'|@start[a-z]+|@end[a-z]+)/,
-        push: [
-          {token: "comment.line.source.wsd", regex: /$/, next: "pop"},
-          {defaultToken: "comment.line.source.wsd"}
-        ]
+        regex: /^@(?:start|end)[^]*$/,
       },
       {
         token: "support.function.source.wsd",
         regex: /\b(?:abstract|actor|class|component|database|enum|interface|object|participant|state|usecase)\b/
-      },
-      {
-        token: "keyword.control.source.wsd",
-        regex: /\btitle\b/,
-        push: [
-          {
-            token: "string.quoted.double.source.wsd",
-            regex: /$/,
-            next: "pop"
-          },
-          {defaultToken: "string.quoted.double.source.wsd"}
-        ]
       },
       {
         token: "keyword.control.source.wsd",
@@ -49,53 +73,6 @@ const CustomHighlightRules = function () {
         token: "variable.source.wsd",
         regex: /\b(?:AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGreen|DarkGrey|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkSlateGrey|DarkTurquoise|DarkViolet|Darkorange|DeepPink|DeepSkyBlue|DimGray|DimGrey|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Green|GreenYellow|Grey|HoneyDew|HotPink|IndianRed|Indigo|Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGreen|LightGrey|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSlateGrey|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|SlateGrey|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen)\b/
       },
-      {
-        token: ["constant.string.source.wsd", "string.quoted.double.source.wsd", "keyword.control.source.wsd", "string.quoted.double.source.wsd", "constant.string.source.wsd", "string.quoted.double.source.wsd", "variable.parameter.source.wsd"],
-        regex: /([A-ZÜÖÄa-z_0-9äöüß]+)( +)(-?->|<-?-)( +)([A-ZÜÖÄa-z_0-9äöüß]+)(:)(.*)/,
-        push: [
-          {
-            token: "string.quoted.double.source.wsd",
-            regex: /$/,
-            next: "pop"
-          },
-          {defaultToken: "string.quoted.double.source.wsd"}
-        ]
-      },
-      {
-        token: "string.quoted.double.source.wsd",
-        regex: /"/,
-        push: [
-          {
-            token: "string.quoted.double.source.wsd",
-            regex: /"/,
-            next: "pop"
-          },
-          {defaultToken: "string.quoted.double.source.wsd"}
-        ]
-      },
-      {
-        token: "string.quoted.single.source.wsd",
-        regex: /'/,
-        push: [
-          {
-            token: "string.quoted.single.source.wsd",
-            regex: /'/,
-            next: "pop"
-          },
-          {defaultToken: "string.quoted.single.source.wsd"}
-        ]
-      },
-      {
-        token: "variable.parameter.source.wsd",
-        regex: /:(.*)/
-      },
-      {
-        token: "constant.string.source.wsd",
-        regex: /\b[A-Z]+[A-ZÜÖÄa-z_0-9äöüß]*\b/
-      },
-      // {token: "variable.parameter.source.wsd", regex: /@startuml|@enduml/}
-      // {token: "variable.parameter.source.wsd", regex: /\b[a-z_]+[A-Za-z_0-9]*\b/}
-      {token: "variable.parameter.source.wsd", regex: /[A-ZÜÖÄa-züöäß_]+[A-ZÜÖÄa-züöäß_0-9\(\)]*/}
     ]
   };
 };
